0921872d420054015704b29f39017205d56fb421
arkadiusz971 <arkadiusz.giemza97@gmail.com>
2022-07-11 15:12:53 +0200

    add password validation with regex pattern, fix error styling

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const pwdConfirm = document.querySelector("#confirm-password")
 pwd.addEventListener("input", e => checkPasswordMatching(e))
 pwdConfirm.addEventListener("input", e => checkPasswordMatching(e))
 
+function setInputInvalid(element) {
+    element.setCustomValidity("Please fill in this field")
+    element.style.borderColor = "red"
+}
+function setInputValid(element) {
+    element.setCustomValidity("")
+    element.style.borderColor = "#0eb041"
+}
+
 function checkPasswordMatching() {
-    if (pwd.value !== pwdConfirm.value) {
-        pwd.setCustomValidity("invalid")
-        pwd.style.borderColor = "red"
-        pwdConfirm.setCustomValidity("invalid")
-        pwdConfirm.style.borderColor = "red"
+    const pwdPattern = /(?=.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/
+    if (!pwd.value.match(pwdPattern) && !pwdConfirm.value.match(pwdConfirm)) {
+        setInputInvalid(pwd)
+        setInputInvalid(pwdConfirm)
+        document.querySelector(".password-error").innerText = "Password must be at least 8 characters long, contain at least: 1 digit, one 1 special character, one uppercase and one lowercase letter"
+    } else if ((pwd.value !== pwdConfirm.value)) {
+        setInputInvalid(pwd)
+        setInputInvalid(pwdConfirm)
         document.querySelector(".password-error").innerText = "Passwords do not match"
+    }
+    else if (pwd.value === "" && pwdConfirm.value === "") {
+        pwd.style.borderColor = "gray"
+        pwdConfirm.style.borderColor = "gray"
+        document.querySelector(".password-error").innerText = ""
     } else {
-        pwd.style.borderColor = "#0eb041"
-        pwdConfirm.style.borderColor = "#0eb041"
+        setInputValid(pwd)
+        setInputValid(pwdConfirm)
         document.querySelector(".password-error").innerText = ""
     }
 }
